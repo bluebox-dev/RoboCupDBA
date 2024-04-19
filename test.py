29705382a9ab4706cc6125c33f5735286e0f397b
Dev <thanakorn.vsalab@gmail.com>
2024-04-19 14:27:16 +0700

    ver_0.1

---
+++ test.py
@@ -0,0 +1,3 @@
+import RoboCupDBA
+# print(RoboCupDBA.Insert(link="mongodb://root:6yHnmju%26@localhost:27017/",db="data",collection="data",name="1",x=0.0,y=0.0,theta=0.0,typeObj="A"))
+# print(RoboCupDBA.Query(link="mongodb://root:6yHnmju%26@localhost:27017/",db="data",collection="data",json={"_id":'1'}))
